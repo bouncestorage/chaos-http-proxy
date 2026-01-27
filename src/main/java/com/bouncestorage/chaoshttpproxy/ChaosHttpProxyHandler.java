db9313fafee0148c00caa8245c7e34203926f515
Andrew Gaul <andrew@gaul.org>
2026-01-27 14:22:46 -0800

    Upgrade to Jetty 11.0.26
    References #46.

--- src/main/java/com/bouncestorage/chaoshttpproxy/ChaosHttpProxyHandler.java
+++ src/main/java/com/bouncestorage/chaoshttpproxy/ChaosHttpProxyHandler.java
@@ -34,8 +34,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.function.Supplier;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.Lists;
--- src/test/java/com/bouncestorage/chaoshttpproxy/ChaosHttpProxyTest.java
+++ src/test/java/com/bouncestorage/chaoshttpproxy/ChaosHttpProxyTest.java
@@ -32,8 +32,8 @@ import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import com.google.common.base.Suppliers;
 import com.google.common.collect.ImmutableList;
